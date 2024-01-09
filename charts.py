05c055053f5970b72eca7f3852ddd4fd94569068
matheuscamposm <matheuscamposmattos@id.uff.br>
2024-01-09 16:58:22 -0300

    create PC and ROC classes:
    - For rendering and handling better the data (OOP)

--- charts.py
+++ charts.py
@@ -1,59 +1,93 @@
 import streamlit as st
-from pydockstats import generate_plots
-from app_utils import plot_curve
+from pydockstats import calculate_curves
 import plotly.graph_objects as go
-def _generate_curves(program_name):
-    scores, activity = st.session_state.data[program_name]['scores'], st.session_state.data[program_name]['activity']
-    pc_data, roc_data = generate_plots(program_name, scores, activity)
-    return pc_data, roc_data
+
+# Functions to plot the curves
+def plot_pc_curve(fig, program_name, curve_data):
+    x, y = curve_data['x'], curve_data['y']
+    auc = curve_data['bedroc']
+    text_legend = f"{program_name}"
+
+    fig.add_trace(
+        go.Scatter(
+            x=x, y=y, mode='lines', 
+            name=text_legend, 
+            line=dict(width=3), showlegend=True,
+            hovertemplate='Quantile: %{x}<br>Activity probability: %{y}<br>'
+        )
+    )
+
+def plot_roc_curve(fig, program_name, curve_data):
+    x, y = curve_data['x'], curve_data['y']
+    auc = curve_data['auc']
+    text_legend = f"{program_name} | AUC={auc:.2f}"
+
+    fig.add_trace(
+        go.Scatter(
+            x=x, y=y, mode='lines', 
+            name=text_legend, 
+            line=dict(width=3), showlegend=True,
+            hovertemplate='FPR: %{x}<br>TPR: %{y}<br>'
+        )
+    )
 
 
+class PredictivenessCurve:
+    def __init__(self):
+        self.name = "PC"
+        st.subheader("Predictiveness Curve")
+        self.__pc_container = st.container()
+        self.__fig = go.Figure(layout=dict(title=dict(text="PC", font=dict(size=20)), xaxis_title="Quantile", yaxis_title="Activity probability",
+                                        legend=dict(orientation="v"), height=600, width=1000, font=dict(family='Montserrat', size=16), legend_title_text="Programs"))
+        self.__fig.update_xaxes(range=[0, 1], constrain='domain', showgrid=False)
+        self.__fig.update_yaxes(range=[0, 1], constrain='domain', showgrid=False)
+    
+    def add_plot(self, program_name):
+        scores, activity = st.session_state.data[program_name]['scores'], st.session_state.data[program_name]['activity']
+        pc_data = calculate_curves(program_name, scores, activity)[0]
+        with self.__pc_container:
+            plot_pc_curve(self.__fig, program_name, pc_data)
+    
+    def render(self):
+        self.__pc_container.plotly_chart(self.__fig)
+
+    def write_image(self, path):
+        self.__fig.write_image(path)
+    
+class ReceiverOperatingCharacteristic:
+    def __init__(self):
+        self.name = "ROC"
+        st.subheader("Receiver Operating Characteristic")
+        self.__roc_container = st.container()
+        self.__fig = go.Figure(layout=dict(title=dict(text="ROC", font=dict(size=20)), xaxis_title="False Positive Rate", 
+                                        yaxis_title="True Positive Rate", legend=dict(orientation="v"), height=600, width=900,
+                                        font=dict(family='Montserrat', size=16), legend_title_text="Programs"))
+        self.__fig.update_xaxes(range=[0, 1], constrain='domain', showgrid=False)
+        self.__fig.update_yaxes(range=[0, 1], constrain='domain', showgrid=False)
+
+    
+    def add_plot(self, program_name):
+        scores, activity = st.session_state.data[program_name]['scores'], st.session_state.data[program_name]['activity']
+        roc_data = calculate_curves(program_name, scores, activity)[1]
+        with self.__roc_container:
+            plot_roc_curve(self.__fig, program_name, roc_data)
+        
+    def render(self):
+        self.__roc_container.plotly_chart(self.__fig)
+
+    def write_image(self, path):
+        self.__fig.write_image(path)
+
+
+        
 
 def generate_charts():
-    st.header("Charts")
-    
-    # Program selection
-    option = st.radio("Select the program", ['All']+st.session_state.programs)
+    pc = PredictivenessCurve()
+    roc = ReceiverOperatingCharacteristic()
+    for program_tab in st.session_state['programs']:
+        pc.add_plot(program_tab.get_program_name())
+        roc.add_plot(program_tab.get_program_name())
 
-    # Plot the PC and the ROC
-    st.subheader("Predictiveness Curve")
-    pc_container = st.container()
-    fig_pc = go.Figure(layout=dict(title=dict(text="PC", font=dict(size=20)), xaxis_title="Quantile", yaxis_title="Activity probability",
-                                    legend=dict(orientation="v"), height=600, width=1000, font=dict(family='Montserrat', size=16), legend_title_text="Programs"))
-    fig_pc.update_xaxes(range=[0, 1], constrain='domain', showgrid=False)
-    fig_pc.update_yaxes(range=[0, 1], constrain='domain', showgrid=False)
-
-
-    
-    
-    st.subheader("Receiver Operating Characteristic")
-    roc_container = st.container()
-    fig_roc = go.Figure(layout=dict(title=dict(text="ROC", font=dict(size=20)), xaxis_title="False Positive Rate", 
-                                    yaxis_title="True Positive Rate", legend=dict(orientation="v"), height=600, width=900,
-                                    font=dict(family='Montserrat', size=16), legend_title_text="Programs"))
-    fig_roc.update_xaxes(range=[0, 1], constrain='domain', showgrid=False)
-    fig_roc.update_yaxes(range=[0, 1], constrain='domain', showgrid=False)
-
-    
-    
-    if option == 'All':
-        for program_name in st.session_state.programs:
-            pc_data, roc_data = _generate_curves(program_name)
-            with pc_container:
-                plot_curve(fig_pc, program_name, pc_data, "Predictiveness Curve")
-
-            with roc_container:
-                plot_curve(fig_roc, program_name, roc_data, "ROC")
-                
-    else:
-        pc_data, roc_data = _generate_curves(option)
-        with pc_container:
-            plot_curve(fig_pc, option, pc_data, "Predictiveness Curve")
-
-        with roc_container:
-            plot_curve(fig_roc, option, roc_data, "ROC")
-        
-    pc_container.plotly_chart(fig_pc)
-    roc_container.plotly_chart(fig_roc)
-
-    return fig_pc, fig_roc
+    pc.render()
+    roc.render()
+    return pc, roc
